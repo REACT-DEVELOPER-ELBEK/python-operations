3a77f4513ff313127d48388d99d4c0b54ea3daf3
unknown <elbeky829@gmail.com>
2023-12-01 23:03:29 +0500

    logic-operations done

---
+++ main.py
@@ -0,0 +1,47 @@
+# word = "elbekYusupovProgrammer"
+# print(word[-5:-1])
+
+# word = "Hi there, Elbek"
+# print(word.upper())
+# print(word.lower())
+
+# word = "     Hi there, Elbek"
+# print(word.strip())
+
+# age = 16
+# name = "Elbek"
+# txt = "My name is {}, I'm {} years old"
+# print(txt.format(name, age))
+
+# name = "You're \"nigga\" "
+# print(name.strip())
+
+# name = "You're \nhuman"
+# print(name)
+
+#tab
+# name = "You're \thuman"
+# print(name)
+
+#back-1
+# name = "You're   \bhuman"
+# print(name)
+
+# name = "You're   \fhuman"
+# print(name)
+
+#search
+# text = "Like damn she in haa mood"
+# print("moo" in text)
+
+# inputVal = input("Write any word or sentence ")
+# search = input("What word do you search? ")
+# if search in inputVal:
+#     print("Sure it has ;)")
+# else:
+#     print("Soory, it doesn't has :(")
+
+# checking for type
+# x = "200.0"
+# print(isinstance(x, str))
+
